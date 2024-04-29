7de2d7427557955dbbf8b4513bb763a1c700a43e
dev-narendra <narendra.bandineni@gmail.com>
2024-04-29 23:09:23 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <div className='flex justify-center text-center items-center bg-cyan-300'>Hey</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+import App from "./App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    }
+])
+
+export default router
