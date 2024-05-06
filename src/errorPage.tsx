f77b764ee006c09fad461b30099de18e6e8a786b
dev-narendra <narendra.bandineni@gmail.com>
2024-05-06 21:35:51 +0530

    t5

--- src/errorPage.tsx
+++ src/errorPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ErrorPage() {
   return (
     <div className='h-auto w-screen flex justify-center items-center'>
-     <div className='flex justify-center text-center items-center bg-cyan-300'>No Page da, Pinne va</div>
+     <div className='flex justify-center text-center items-center bg-cyan-300'>No Page da, Pinne va, ok na</div>
     </div>
   );
 }
