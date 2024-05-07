54552a5e42885076e8ca47a63f0d9f620c4cfd95
dev-narendra <narendra.bandineni@gmail.com>
2024-05-07 21:59:17 +0530

    c1

--- src/errorPage.tsx
+++ src/errorPage.tsx
@@ -5,7 +5,7 @@ function ErrorPage() {
     <div className='h-auto w-screen flex justify-center items-center'>
      <div className='flex justify-center text-center items-center bg-cyan-300'>No Page da, Pinne va, ok na</div>
     </div>
-  );
+  ); //Adding Comment
 }
 
 export default ErrorPage;
